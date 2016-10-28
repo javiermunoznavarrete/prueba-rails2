ed13ddc5ceba995c8de2f5d04d154f6b8d3fcdfd
javiermunoznavarrete <javier_munoz_navarrete@hotmail.com>
2016-10-28 20:43:07 -0300

    tercer comit de la prueba

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
 	has_many :item
+
+	validates :name, presence: true
+	validates :email, uniqueness: true
+
+	before_save :first_mayus
+
+	def first_mayus
+		:name.capitalize
+	end
 end
